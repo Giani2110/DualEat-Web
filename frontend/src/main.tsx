c0266e394df2b5f6edb79a1553cb00639c216603
Giani2110 <gianfranco.andreachi@davinci.edu.ar>
2025-08-06 16:35:43 -0300

    feat: add Arvo Font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './tailwind.css'
 import './index.scss'
+import './App.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
