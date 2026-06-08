35fb9c1e5b35f77325a615e12b9e42a28e975abc
Galoniax <axelian2504@gmail.com>
2026-06-07 21:53:40 -0300

    Style: Fonts & assets
    - Se alteró la manera de importar fuentes de texto y se actualizó todo los archivos (.scss/.css).

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,29 @@
-/ @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src//*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "text-1": "#ffffff",
+        "text-2": "#dbdbdb",
+        "text-3": "#2F2F2F",
+        "text-4": "#707070",
+        "text-5": "#4A4947",
+        "text-6": "#878787",
+        "bg-red": "#B53325",
+        "bg-yellow": "#e5a657",
+        "bg-semi-black": "#333333",
+
+        "bg-gray": "#f5f5f5",
+        "bg-semi-white": "#fefefe",
+        "bg-blue": "#3578e4",
+        "bg-blue-black": "#054FAF",
+      },
+
       fontFamily: {
-        dosis: ['Dosis', 'sans-serif'],
+        Outfit: ["Outfit Variable", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
